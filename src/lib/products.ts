649a44de5e303d8d2099f3229208e4cf47e8cdaf
ppkyon <ppkyon@gmail.com>
2025-11-10 14:26:58 +0900

    商品名、値段変更

--- src/lib/products.ts
+++ src/lib/products.ts
@@ -1,7 +1,7 @@
 export type Product = { id: string; name: string; price: number };
 
 export const PRODUCTS: Product[] = [
-    { id: 'p1', name: '国産ベジプロ', price: 500 },
-    { id: 'p2', name: '無添加ドライデーツ', price: 800 },
-    { id: 'p3', name: 'マンダリンオレンジ缶', price: 700 },
+    { id: 'p1', name: 'ねこのごはん', price: 2970 },
+    { id: 'p2', name: 'ねこのおやつ', price: 864 },
+    { id: 'p3', name: 'ねこのはみがき', price: 1296 },
 ];
